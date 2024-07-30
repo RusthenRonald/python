689d30b7df66ed2427e5f6dea2047265c6743840
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-07-30 00:33:19 -0300

    ex011 concluido

---
+++ exercicios/ex011.py
@@ -0,0 +1,5 @@
+larg=float(input("Digite a largura da parede:"))
+altura=float(input("Digite a altura da parede:"))
+area=larg*altura
+print(f"Com a media {larg}x{altura} sua área seá de {area}m")
+print(f"A quantidade de tinta necessária será de {area/2:.2f}")
