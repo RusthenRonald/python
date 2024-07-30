a753574b38493a48ec7748108e8c5af74f91f906
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-07-30 00:52:38 -0300

    ex015 concluido

---
+++ exercicios/ex015.py
@@ -0,0 +1,4 @@
+km=int(input("Digite a quantidade de km percorridos:"))
+dias=int(input("Por quantos dias ele foi alugado?"))
+total=(60*dias)+(0.15*km)
+print(f"Com {km}km percorridos por {dias} dias , o total a pagar será de {total}R$")
