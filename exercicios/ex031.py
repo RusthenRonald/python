16b560ab4d359359c7c92f0df00207c8bb11a36f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-07-31 00:29:26 -0300

    ex031 concluido

---
+++ exercicios/ex031.py
@@ -0,0 +1,6 @@
+n1=int(input("Digite um número:"))
+n2=int(input("Digite um número:"))
+n3=int(input("Digite um número:"))
+lista=[n1,n2,n3]
+print(f"O maior número é {max(lista)}")
+print(f"O menor número é {min(lista)}")
