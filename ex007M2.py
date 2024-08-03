4763a1a020113a8ecd829c57a293ef5d18d2f713
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-08-03 00:29:48 -0300

    terminei exercicio

---
+++ ex007M2.py
@@ -0,0 +1,14 @@
+peso=float(input("Peso:"))
+altura=float(input("Altura:"))
+imc=peso/(altura**2)
+print(f"Seu imc é de {imc:.2f}")
+if imc < 18.5:
+    print("Abaixo do peso")
+elif imc >=18.5 and imc <25:
+    print("Peso ideal")
+elif imc >=25 and imc <30:
+    print("Sobrepeso")
+elif imc >=30 and imc <40:
+    print("Obesidade")
+else:
+    print("Obesidade mórbida")
