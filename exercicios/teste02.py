9fd745900c9a44f867e271942e70426105cdf9c1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-08-06 16:01:31 -0300

    atualizei exercicio

--- exercicios/teste02.py
+++ exercicios/teste02.py
@@ -2,13 +2,18 @@ import random
 lista_numeros_escolhidos=[]
 while True:
     quantidade_numeros=int(input("Digite a quantidade de números que você deseja marcar:"))
-    if quantidade_numeros >=15 and quantidade_numeros<=18:
+    if quantidade_numeros in [15,16,17,18]:
         break
     else:
-        print("Digite apenas números entre 15 e 18!")
-    for c in range(0,quantidade_numeros):
-        numeros_escolhidos=int(input("Digite o número escolhido:"))
-        if numeros_escolhidos in lista_numeros_escolhidos:
-            print("Esse número já está na lista!")
-        else:
-            lista_numeros_escolhidos.append(numeros_escolhidos)
+        print("Digite apenas números entre 15 e 18")
+
+while len(lista_numeros_escolhidos) <quantidade_numeros:#verificação de números dentro da lista
+    numeros_escolhidos=int(input("Digite os números escolhidos:"))
+    if numeros_escolhidos <1 or numeros_escolhidos>25:#validação de número de 1 até 25
+        print("Digite apenas números entre 1 e 25")
+    elif numeros_escolhidos not in lista_numeros_escolhidos:#verificação de números repetidos
+        lista_numeros_escolhidos.append(numeros_escolhidos)
+    else:
+        print("Esse número já está na lista , não vou adcionar!")
+print(f"O usuário escolheu esses números:{lista_numeros_escolhidos}")
+print("Fim")
