bd3f76b9830678d6a49e0fc4114c27b12c8c9056
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-24 16:22:49 -0300

    finalizei exercicio

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,3 +1,16 @@
 valores=[]
+while True:
+    valores.append(int(input("Digite um número:")))
+    r=str(input("Quer continuar?")).strip().upper()
+    if r in ["N","NAO","NÂO"]:
+        break
 
+print(f"Foram digitados {len(valores)} números")
+if 5 not in valores:
+    print("O número 5 não foi digitado!")
+else:
+    posicao=valores.index(5)
+    print(f"O valor 5 foi digitado e está na posição {posicao+1}° ")
+valores.sort(reverse=True)
+print(valores)
 print("Fim")
