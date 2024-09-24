95f419b60332391632ad7a85c66fe149e41201bf
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-24 16:05:56 -0300

    deletei

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,14 +1,3 @@
 valores=[]
-while True:
-    num=int(input("Digite um número:"))
-    if num not in valores:
-        valores.append(num)
-    else:
-        print("Número repetido! \nnão vou adcionar")
-    r=str(input("Quer continuar?")).strip().upper()
-    if r in ["N","NÂO","NAO"]:
-        break
-print("Você digitou os números:",end=" ")
-valores.sort()
-print(valores)
+
 print("Fim")
