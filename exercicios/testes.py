414d9f598c3c521fc4ffda678ff17fc5d77edbee
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-26 15:09:36 -0300

    terminei exercicio

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,3 +1,6 @@
 palavras = ("gato", "cachorro", "passaro", "leão", "elefante", "tigre")
 for p in palavras:
-    print(f" Na palavra {p.upper()} temos ")
+    print(f"\n Na palavra {p.upper()} temos ",end=" ")
+    for l in p :
+        if l in "AaEeIiOoUu":
+            print(l ,end=" ")
