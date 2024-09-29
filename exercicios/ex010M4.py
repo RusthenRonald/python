89413e713611beed2b240f9e3f89d87784e80c67
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-29 02:30:45 -0300

    criei a soma dos valores pares com matriz

--- exercicios/ex010M4.py
+++ exercicios/ex010M4.py
@@ -1,4 +1,5 @@
 matriz=[[0,0,0],[0,0,0],[0,0,0]]
+soma_pares=maior=soma_coluna=0
 pares=[]
 for l in range(0,3):
     for c in range(0,3):
@@ -6,5 +7,8 @@ for l in range(0,3):
 for l in range(0,3):
     for c in range(0,3):
         print(f"[{matriz[l][c]:^5}]",end=" ")
+        if matriz[l][c]%2==0:
+            pares.append(matriz[l][c])
+            soma_pares+=matriz[l][c]
     print()
-print(f"A soma dos valores pares é {pares}")
+print(f"A soma dos valores pares é {soma_pares}")
