3f2e8b8e477e836bb3780c8e70c041c936418473
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-29 21:34:54 -0300

    fiz a verificação dos números pares

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,1 +1,12 @@
+soma_pares=0
 matriz=[[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c]=int(input(f"Digite um número para[{l},{c}]:"))
+for l in range(0,3):
+    for c in range(0,3):
+        print(f"[{matriz[l][c]:^5}]",end=" ")
+        if matriz[l][c]%2==0:
+            soma_pares+=matriz[l][c]
+    print()
+print(f"Foram digitados {soma_pares} números pares ")
