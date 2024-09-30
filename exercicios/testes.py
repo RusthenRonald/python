279fbe5eff2dfc9efcc79a0f65425df1ad008e94
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-29 21:50:24 -0300

    terminei exercicio

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,4 +1,4 @@
-soma_pares=soma_coluna=0
+soma_pares=soma_coluna=maior=0
 matriz=[[0,0,0],[0,0,0],[0,0,0]]
 for l in range(0,3):
     for c in range(0,3):
@@ -11,5 +11,12 @@ for l in range(0,3):
     print()
 for l in range(0,3):
     soma_coluna+=matriz[l][1]
+for c in range(0,3):
+    if c ==0:
+        maior=matriz[1][c]
+    else:
+        if matriz[1][c]>maior:
+            maior=matriz[1][c]
 print(f"Foram digitados {soma_pares} números pares ")
 print(f"A soma dos números da 1 coluna foi de {soma_coluna} ")
+print(f" O maior valor da segunda linha foi de {maior}")
