e4933795383ff494e7103f2e7194c50167ed2cef
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-29 22:22:48 -0300

    finalizei exercicio

---
+++ exercicios/ex011M4.py
@@ -0,0 +1,15 @@
+from random import sample
+from time import sleep
+cont=0
+print("-="*10)
+print("JOGA NA MEGASENA")
+print("-="*10)
+sorteio=[]
+quant_jogos=int(input("Digite a qauntidade de jogos :"))
+for c in range(0,quant_jogos):
+    computador=sample(range(1, 61), 6)
+    sorteio.append(computador)
+for s in sorteio:
+    sleep(0.5)
+    print(f" Jogo {cont+1}° {s}")
+    cont+=1
