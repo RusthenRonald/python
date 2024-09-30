0af49325cb41e9827971e6094d4369a77f1a55ec
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-09-30 16:20:31 -0300

    terminei exercício

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,1 +1,11 @@
-listagem=()
+listagem=( "Arroz", 5.99,
+    "Feijão", 7.49,
+    "Macarrão", 3.29,
+    "Leite", 4.69,
+    "Açúcar", 2.99,
+    "Café", 9.89)
+for item in range(0,len(listagem)):
+    if item%2==0:
+        print(f"{listagem[item]:.<30}",end="")
+    if item%2==1:
+        print(f"R${listagem[item]:>5}")
