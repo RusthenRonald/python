51ab96640e129cb58d34cc85faf8bd682b99355f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-03 01:05:57 -0300

    adcionei novo dicionário

--- exercicios/ex002dict.py
+++ exercicios/ex002dict.py
@@ -1,6 +1,9 @@
 from random import randint
+from time import sleep
 result={}
+rank={}
 for c in range(0,5):
     result["jogador"]=randint(0,10)
-    print(f"jogador{c} tirou {result['jogador']}")
+    print(f"jogador{c} tirou {result['jogador']} no dado")
+    sleep(0.8)
 print("== RANKING DOS JOGADORES ==")
