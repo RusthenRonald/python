640b0c333c82cdb23854f2679aad9cb5c7d94f78
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-03 01:42:30 -0300

    exercício concluido

--- exercicios/ex002dict.py
+++ exercicios/ex002dict.py
@@ -1,10 +1,17 @@
 from random import randint
 from time import sleep
-result={}
+from _operator import itemgetter
+result={
+    "jogador1":randint(0,6),
+    "Jogador2":randint(0,6),
+    "jogador3":randint(0,6),
+    "jogador4":randint(0,6)
+}
 rank={}
-for c in range(0,5):
-    result["jogador"]=randint(0,10)
-    print(f"jogador{c} tirou {result['jogador']} no dado")
+for k,v in result.items():
+    print(f"{k}:{v}")
     sleep(0.8)
-rank=sorted(result.items())
+rank=sorted(result.items(),key=itemgetter(1),reverse="True")
 print("== RANKING DOS JOGADORES ==")
+for i,v in enumerate(rank):
+    print(f"{i}° lugar: {v[0]} com {v[1]} pontos")
