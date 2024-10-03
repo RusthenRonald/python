43e31da7bb358cd0c6bee1a0a19bd8149ca21186
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-03 01:11:11 -0300

    adcionei sorted

--- exercicios/ex002dict.py
+++ exercicios/ex002dict.py
@@ -6,4 +6,5 @@ for c in range(0,5):
     result["jogador"]=randint(0,10)
     print(f"jogador{c} tirou {result['jogador']} no dado")
     sleep(0.8)
+rank=sorted(result.items())
 print("== RANKING DOS JOGADORES ==")
