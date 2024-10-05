3688c23f2fd71171060f9537342f488d5133fb6f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-04 23:59:32 -0300

    testando condicionais

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,5 +1,8 @@
 dados={}
 dados["Nome"]=str(input("Nome:"))
 dados["Média"]=float(input("Mèdia:"))
+if dados["situaçao"]>=7 :
+    dados["situaçao"]="Aprovado"
 for k,v in dados.items():
     print(f"{k} :{v} ")
+
