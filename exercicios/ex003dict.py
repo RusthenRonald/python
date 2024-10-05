89c176979a3cc699cf479647b7f1fd43555a69a6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-05 01:10:38 -0300

    finalizei

--- exercicios/ex003dict.py
+++ exercicios/ex003dict.py
@@ -1,10 +1,15 @@
 from datetime import date
 dados={}
 ano_atual=date.today().year
-
+aposentadoria=70
 dados["Nome"]=str(input("Nome:"))
-dados["Ano de Nascimento"]=int(input("Ano de Nascimento:"))
+ano_nasc=int(input("Ano de Nascimento:"))
+dados["Idade"]=ano_atual-ano_nasc
 dados["Carteira de Trabalho"]=int(input("Carteira de Trabalho (0 não tem):"))
+if dados["Carteira de Trabalho"]!=0:
+    dados["Ano de contratação"]=int(input("Ano de contratação:"))
+    dados["Salário"]=float(input("Salário:"))
+    dados["Aposentadoria"]=aposentadoria-dados["Idade"]
 
 for k,v in dados.items():
     print(f"- {k} {v}")
