e9b3b316f2e135bc276036b5566e4c27412005e8
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-05 00:29:09 -0300

    concluido

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,6 +1,12 @@
 from random import randint
+from operator import itemgetter
 resultados={}
 for c in range(0,4):
     resultados[f"Jogador_{c+1}"]=randint(0,6)
 for k,v in resultados.items():
     print(f"{k} tirou {v} no dado.")
+rank=[]
+print("RANKING DOS JOGADORES")
+rank=sorted(resultados.items(),key=itemgetter(1),reverse=True)
+for i,v in enumerate(rank):
+    print(f"{i+1}° lugar o {v[0]} com {v[1]}.")
