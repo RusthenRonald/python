94b7e5259a0a68c439093f879dddd302863121ee
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-09 15:34:39 -0300

    finalizei

--- exercicios/teste.py
+++ exercicios/teste.py
@@ -1,4 +1,11 @@
 dados={}
 dados["Nome"]=str(input("Nome:"))
 dados["Média"]=float(input("Média:"))
-print(dados)
+if dados["Média"]>=7:
+    dados["Situação"]="Aprovado"
+elif dados["Média"]>=5 and dados["Média"]<7:
+    dados["Situação"]="Recuperção"
+else:
+    dados["Situação"]="Reprovado"
+for k,v in dados.items():
+    print(f"- {k} é igual á {v}")
