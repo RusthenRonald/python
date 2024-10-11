79cf100f3307a8649bfd518048b29da6af733868
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-11 02:12:06 -0300

    fiz o cálculo de idade na lista

--- exercicios/ex005dict.py
+++ exercicios/ex005dict.py
@@ -1,14 +1,18 @@
 dados={}
+principal=[]
+soma_idade=0
 while True:
     dados["Nome"]=str(input("Nome:"))
-    dados["Sexo"]=str(input("Sexo")).strip().upper()
+    dados["Sexo"]=str(input("Sexo:")).strip().upper()
 
     while dados["Sexo"] not in ["M","F"]:#validação do sexo
         print("ERRO!, Por favor , digite apenas M ou F .")
         dados["Sexo"]=str(input("Sexo")).strip().upper()
 
-    dados["Idade"]=int(input("Idade"))
-
+    dados["Idade"]=int(input("Idade:"))
+    principal.append(dados.copy())
+    soma_idade+=dados["Idade"]
+    media=soma_idade/len(principal)
     r=str(input("Quer continuar?")).strip().upper() # parada do while
     if r in ["NÂO","N","NAO"]:
         break
@@ -16,4 +20,6 @@ while True:
         print("ERRO! , Digite apenas S ou N")
         r=str(input("Quer continuar?")).strip().upper()
 print("Fim")
+print(f"Foram cadastradas {len(principal)} pessoas.")
+print(f"A média de idade é de {media} anos.")
 
