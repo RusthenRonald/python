da719f0a2cb6bd662b9fab17012dde04cd66d3be
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-11 00:15:54 -0300

    criei uma nova lista para exercício

--- exercicios/ex004dict.py
+++ exercicios/ex004dict.py
@@ -2,21 +2,16 @@ jogador={}
 jogador["Nome"]=str(input("Nome do jogador:"))
 partidas=int(input(f"Quantas partidas {jogador['Nome']} jogou "))
 c=total=0
-part_gol=[]
+gols=[]
 while c != partidas:
     jogador["Gols"]=int(input(f"Quantos gols na partida {c+1}?"))
     total=total+jogador["Gols"]
-    part_gol.append(jogador)
+    gols.append(jogador["Gols"])
     c+=1
 jogador["Total"]=total
 
 print(jogador)
 
 print("=-"*15)
-for k , v in jogador.items():
-    print(f"O campo {k} tem o valor {v}")
-
-print("=-"*10)
-print(f"O jogador {jogador['Nome']} jogou {partidas} partidas")
-print(part_gol)
+print(f"O campo gols tem o valor {gols} ")
 
