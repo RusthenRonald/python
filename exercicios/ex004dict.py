af47b5314e0b43549db749fd674fa94791838950
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-11 00:27:27 -0300

    conclui exercício

--- exercicios/ex004dict.py
+++ exercicios/ex004dict.py
@@ -9,9 +9,21 @@ while c != partidas:
     gols.append(jogador["Gols"])
     c+=1
 jogador["Total"]=total
-
-print(jogador)
-
 print("=-"*15)
-print(f"O campo gols tem o valor {gols} ")
+print(jogador)
+print("=-"*15)
+
+del(jogador["Gols"])
+
+for k,v in jogador.items():
+    print(f"O campo {k} tem o valor {v}")
+print(f"O campo gols tem o valor {gols}")
+print("=-"*15)
+
+print(f"O jogador {jogador['Nome']} jogou {partidas} partidas.")
+for c , v in enumerate(gols):
+    print(f"Na partida {c}, fez {v} gols")
+
+
+
 
