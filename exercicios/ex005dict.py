ee39c2a088b96709f33613c882960d08bf885ef7
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-11 01:34:03 -0300

    fiz a validação de saida

--- exercicios/ex005dict.py
+++ exercicios/ex005dict.py
@@ -1,9 +1,19 @@
 dados={}
 while True:
     dados["Nome"]=str(input("Nome:"))
-    dados["Sexo"]=str(input("Sexo"))
+    dados["Sexo"]=str(input("Sexo")).strip().upper()
+
+    while dados["Sexo"] not in ["M","F"]:#validação do sexo
+        print("ERRO!, Por favor , digite apenas M ou F .")
+        dados["Sexo"]=str(input("Sexo")).strip().upper()
+
     dados["Idade"]=int(input("Idade"))
-    r=str(input("Quer continuar?")).strip().upper()
+
+    r=str(input("Quer continuar?")).strip().upper() # parada do while
     if r in ["NÂO","N","NAO"]:
         break
-    
+    while r not in ["S","N"]: # validação de saida
+        print("ERRO! , Digite apenas S ou N")
+        r=str(input("Quer continuar?")).strip().upper()
+print("Fim")
+
