8d46665dcde78cb0012c4b3595307592f2cdb35a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-12 01:36:25 -0300

    troquei a variável de lugar

--- exercicios/ex005dict.py
+++ exercicios/ex005dict.py
@@ -12,13 +12,13 @@ while True:
     dados["Idade"]=int(input("Idade:"))
     principal.append(dados.copy())
     soma_idade+=dados["Idade"]
-    media=soma_idade/len(principal)
     r=str(input("Quer continuar?")).strip().upper() # parada do while
     if r in ["NÂO","N","NAO"]:
         break
     while r not in ["S","N"]: # validação de saida
         print("ERRO! , Digite apenas S ou N")
         r=str(input("Quer continuar?")).strip().upper()
+media=soma_idade/len(principal)
 print("Fim")
 print(f"Foram cadastradas {len(principal)} pessoas.")
 print(f"A média de idade é de {media} anos.")
