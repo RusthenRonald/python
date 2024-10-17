a4d342ca04d356903fa1866cd508b289284c0746
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-17 00:09:08 -0300

    fiz validação

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,1 +1,11 @@
 dados={}
+while True:
+    dados["Nome"]=str(input("Nome:"))
+    dados["Sexo"]=str(input("Sexo:"))
+    dados["Idade"]=int(input("Idade:"))
+    r=str(input("Quer continuar?")).upper().strip()
+    while r not in ["S","N"]:
+        r=str(input("Quer continuar?")).upper().strip()
+    if r in "N":
+        break
+print("fim")
