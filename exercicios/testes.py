7b3842a7216b9d2edeb89ceabbcf911632bded1e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-17 00:24:05 -0300

    atualizei

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,11 +1,20 @@
 dados={}
+principal=[]
 while True:
     dados["Nome"]=str(input("Nome:"))
-    dados["Sexo"]=str(input("Sexo:"))
+
+    dados["Sexo"]=str(input("Sexo:")).upper().strip()
+    while dados["Sexo"] not in ["M","F"]:
+            dados["Sexo"]=str(input("Sexo:")).upper().strip()
     dados["Idade"]=int(input("Idade:"))
+
     r=str(input("Quer continuar?")).upper().strip()
     while r not in ["S","N"]:
+        print("Digite apenas (S) ou (N)")
         r=str(input("Quer continuar?")).upper().strip()
+    principal.append(dados.copy())
+    dados.clear()
     if r in "N":
         break
-print("fim")
+
+print(f"Foram cadastradas {len(principal)} pessoas")
