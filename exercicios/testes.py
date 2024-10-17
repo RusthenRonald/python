818647cd2143d31561c02e12a67bc708af39dd77
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-17 16:00:56 -0300

    atualizei

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -1,20 +1,20 @@
 jogador={}
 principal=[]
-gols=[]
-jogador["Nome"]=str(input("Nome do jogador:"))
-quant_partidas=int(input(f"Quantas partidas {jogador['Nome']} jogou:"))
-for c in range(0,quant_partidas):
-    quant_gols=int(input(f"Quantos gols na partida {c} :"))
-    gols.append(quant_gols)
-jogador["Gols"]=gols
-total=sum(gols)
-jogador["Total"]=total
-print(jogador)
+while True:
+    jogador["Nome"]=str(input("Nome do jogador:"))
+    quant_partidas=int(input(f"Quantas partidas {jogador['Nome']} jogou:"))
+    gols=[]
+    for c in range(0,quant_partidas):
+        quant_gols=int(input(f"Quantos gols na partida {c} :"))
+        gols.append(quant_gols)
+    jogador["Gols"]=gols
+    total=sum(gols)
+    jogador["Total"]=total
+    principal.append(jogador.copy())
+    r=str(input("Quer continuar?")).upper()
+    if r in "N":
+        break
 print("="*15)
-for k,v in jogador.items():
-    print(f"O campo {k} tem o valor {v}")
-print("="*15)
-print(f"O jogador {jogador['Nome']} jogou {quant_partidas} partidas.")
-for i,v in enumerate(gols):
-    print(f"Na partida {i}, fez {v} gols.")
-print(f"Foi um total de {total} gols")
+print(f"{"Num":^5} {"Gols":^5} {"Total":^10}")
+for i,v in enumerate(principal):
+    print(f"{i} {v["Nome"]} {v["Gols"]} {v["Total"]}")
