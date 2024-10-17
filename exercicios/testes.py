e686e8ef27ee8eb812f090f4ca8dd3254d5a56ee
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-10-17 16:25:31 -0300

    atualizei

--- exercicios/testes.py
+++ exercicios/testes.py
@@ -18,3 +18,15 @@ print("="*15)
 print(f"{"Num":^5} {"Gols":^5} {"Total":^10}")
 for i,v in enumerate(principal):
     print(f"{i} {v["Nome"]} {v["Gols"]} {v["Total"]}")
+while True:
+    opc=int(input("Mostrar dados de qual jogador? (999 para interromper):"))
+    if opc<=len(jogador):
+        jogador_selecionado=principal[opc]
+        print(f"Levantamento jogador {jogador[opc]["Nome"]}")
+        for i,v in enumerate(jogador_selecionado["Gols"]):
+            print(f"No jogo {i} fez {v} gols")
+    else:
+        print("Não existe em nosso sistema")
+    if opc == 999: 
+        print("Encerrando...")
+        break
