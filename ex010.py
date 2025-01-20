8b8a07517f2ce364a9a097e7ab5749a114061390
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-20 00:04:54 -0300

    calculando porcentagem

---
+++ ex010.py
@@ -0,0 +1,5 @@
+larg=float(input("Digite a largura da parede:"))
+alt=float(input("Digite a altura da parede:"))
+area=larg*alt
+quant_tinta=area/2
+print(f"A dimensão é {larg}x{alt} e sua área é de {area}mm \n A quantidade de tinta necessária será de {quant_tinta}l de tinta")
---
+++ ex011.py
@@ -0,0 +1,3 @@
+preco_prod=float(input("Qual o preço do produto? R$"))
+desconto=preco_prod-(preco_prod*5/100)
+print(f"O produto que custava {preco_prod}r$ com 5% de desconto passara a ficar {desconto}")
