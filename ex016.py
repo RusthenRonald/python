f46939eec893d4d2c4ba5b2f13d2aab83fa12875
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-20 15:36:49 -0300

    calculei a hipotenusa

---
+++ ex016.py
@@ -0,0 +1,4 @@
+co=float(input("Digite o cateto oposto:"))
+ca=float(input("Digite o cateto adjacente:"))
+hipo=((ca**2) + (co**2))**(0.5)
+print(f"Com ca {ca} e co {co} a hipotenusa será de {hipo:.2f}")
