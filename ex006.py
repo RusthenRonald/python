b4d346c4e6462339968e9fe5dd91673f0447f38b
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-19 23:10:54 -0300

    adicionando casas flutuantes

--- ex006.py
+++ ex006.py
@@ -4,4 +4,4 @@ triplo=dobro*3
 raiz= num**(1/2)
 print(f"O dobro de {num} é {dobro}")
 print(f"O triplo de {num} e {triplo}")
-print(f"A raiz de {num} é {raiz}")
+print(f"A raiz de {num} é {raiz:.0f}")
