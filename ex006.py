a3a81cc8d320ae3dae1f3e148a1a9a75fa9f737f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-19 23:07:29 -0300

    finalizei raiz quadrada

--- ex006.py
+++ ex006.py
@@ -1,8 +1,7 @@
-import math
 num=int(input("Digite um número:"))
 dobro=num*2
 triplo=dobro*3
-raiz= math.sqrt(num)
+raiz= num**(1/2)
 print(f"O dobro de {num} é {dobro}")
 print(f"O triplo de {num} e {triplo}")
 print(f"A raiz de {num} é {raiz}")
