4db9eea7e3733da6929a77d5c55aa71de832c716
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-20 15:18:07 -0300

    finalizei exercicio

---
+++ ex014.py
@@ -0,0 +1,4 @@
+quant_km=int(input("Digite a quantidade de Km percorridos pelo carro:"))
+dias= int(input("Digite a quantidade de dias:"))
+preco=(dias*60)+(quant_km*0.15)
+print(f"Com {quant_km}Km percorridos e com {dias} dias alugados ele ira pagar um total de {preco}R$")
