40a824e3d3c47583d1e5d8f83e9b4ce43d93a19e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-21 16:21:21 -0300

    finalizei exercicio

--- ex030.py
+++ ex030.py
@@ -1,4 +1,7 @@
 sal=float(input("Digite o salário do funcionário:"))
 if sal>1250 :
     aumento=sal+(sal*10/100)
-    print(fO funcionário que ganhava ())
+    print(f"O funcionário que ganhava {sal}R$ passará a ganhar {aumento}R$")
+elif sal<=1250:
+    aumento=sal+(sal*15/100)
+    print(f"O funcionário que ganhava {sal}R$ passará a ganhar {aumento}R$")
