474b3203e961b91235608536afbfbd1058be3a7f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-22 16:31:30 -0300

    classificando atletas com condicionais

---
+++ ex036.py
@@ -0,0 +1,19 @@
+import datetime
+nasc=int(input("Digite seu ano de nascimento:"))
+ano_atual=datetime.datetime.now().year
+idade=ano_atual-nasc
+if idade<=9:
+    print(f"O atleta tem {idade} anos")
+    print("Classificação : Mirim")
+elif idade>9 and idade <=14:
+    print(f"O atleta tem {idade} anos")
+    print("Classificação : Infantil")
+elif idade>14 and idade<=19:
+    print(f"O atleta tem {idade} anos")
+    print("Classificação : Junior")
+elif idade>19 and idade<=25:
+    print(f"O atleta tem {idade} anos")
+    print("Classificação : Sênior")
+else:
+    print(f"O atleta tem {idade} anos")
+    print("Classificação : Master")
