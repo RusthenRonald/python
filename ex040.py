08093e25504f7993c6f08feb20a0686440c46bff
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-23 17:30:26 -0300

    fiz condições de papel

--- ex040.py
+++ ex040.py
@@ -11,12 +11,27 @@ time.sleep(1.4)
 print("KEN")
 time.sleep(1.4)
 print("PO")
+#pedra
 if jogador == 0 and computador == 0:
     print("EMPATE")
     print("Ambos escolheram PEDRA")
+if jogador == 0 and computador == 1:
+    print("DEORROTA")
+    print("O jogador escolheu PEDRA e o computador PAPEL")
+if jogador == 0 and computador ==2:
+    print("VITORIA")
+    print("O jogador escolheu PEDRA e o computador TESOURA")
+#papel
 if jogador == 1 and computador ==1:
     print("EMPATE")
     print("Ambos escolheram PAPEL")
+if jogador == 1 and computador == 0:
+    print("VITORIA")
+    print("O jogador escolheu PAPEL e o computador PEDRA")
+if jogador == 1 and computador == 2:
+    print("DEORROTA")
+    print("O jogador escolheu PAPEL e o computador TESOURA")
+#tesoura
 if jogador == 2 and computador == 2:
     print("EMPATE")
     print("Ambos escolheram TESOURA")
