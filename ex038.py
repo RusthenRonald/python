3ba69f502558dc523567cb31f68fea5b7cd7e82f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-23 15:24:02 -0300

    finalizei programa

--- ex038.py
+++ ex038.py
@@ -2,3 +2,13 @@ print("Calculando IMC")
 peso=float(input("Digite o seu peso:"))
 alt=float(input("Digite sua altura:"))
 imc=peso/(alt**2)
+if imc<18.5:
+    print(f"Seu imc é de {imc:.2f} \n Categoria: Abaixo do peso")
+elif imc>=18.5 and imc <25:
+     print(f"Seu imc é de {imc:.2f} \n Categoria: Peso ideal")
+elif imc>=25 and imc <30:
+      print(f"Seu imc é de {imc:.2f} \n Categoria: Sobre peso")
+elif imc>=30 and imc<40:
+      print(f"Seu imc é de {imc:.2f} \n Categoria: Obesidade")
+else:
+      print(f"Seu imc é de {imc:.2f} \n Categoria: Obesidade mórbida")
