a55000137acd004062d90e663c3685c13b2c7bc2
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-27 01:42:02 -0300

    finalizei programa

--- ex050.py
+++ ex050.py
@@ -1,6 +1,6 @@
-tot_m=media=soma_idade=tot_p=maior_idade=0
+tot_m=media=soma_idade=tot_p=maior_idade=nome_velho=0
 for c in range(1,5):
-    print(f"{c+1}° Pessoa")
+    print(f"{c}° Pessoa")
     tot_p+=1
     nome=str(input("Nome:"))
     idade=int(input("Idade:"))
@@ -12,10 +12,12 @@ for c in range(1,5):
     if sexo in ["M","MASCULINO"]:
         if c ==1:
             maior_idade=idade
+            nome_velho=nome
         else:
             if idade>maior_idade:
                 maior_idade=idade
+                nome_velho=nome
     print("=-"*5)
 print(f"Tem {tot_m} mulheres com menos de 20 anos")
 print(f"A media de idade do grupo é de {media} anos")
-print(f"A maior idade é de {maior_idade}")
+print(f"A maior idade é de {maior_idade} anos e o nome da pessoa é {nome_velho}")
