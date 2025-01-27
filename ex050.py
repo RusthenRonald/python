d11e24843b4c13b2e3ac615590a8d8cedac420d7
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-27 01:23:52 -0300

    contador de mulheres com menos de 20 anos

--- ex050.py
+++ ex050.py
@@ -3,7 +3,8 @@ for c in range(0,5):
     print(f"{c+1}° Pessoa")
     nome=str(input("Nome:"))
     idade=int(input("Idade:"))
-    sexo=str(input("Sexo:")).strip().upper()
-    if sexo in ["M","MULHER","FEMININO","F"] and idade<20:
+    sexo=str(input("Sexo: (M/F)")).strip().upper()
+    if sexo in ["FEMININO","F"] and idade<20:
         tot_m+=1
     print("=-"*5)
+print(f"Tem {tot_m} mulheres com menos de 20 anos")
