1c2b10281cb912bd8975dac76fd55c9deae34b61
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-27 01:37:11 -0300

    calculando maior idade

--- ex050.py
+++ ex050.py
@@ -1,4 +1,4 @@
-tot_m=media=soma_idade=tot_p=0
+tot_m=media=soma_idade=tot_p=maior_idade=0
 for c in range(1,5):
     print(f"{c+1}° Pessoa")
     tot_p+=1
@@ -9,6 +9,13 @@ for c in range(1,5):
     sexo=str(input("Sexo: (M/F)")).strip().upper()
     if sexo in ["FEMININO","F"] and idade<20:
         tot_m+=1
+    if sexo in ["M","MASCULINO"]:
+        if c ==1:
+            maior_idade=idade
+        else:
+            if idade>maior_idade:
+                maior_idade=idade
     print("=-"*5)
 print(f"Tem {tot_m} mulheres com menos de 20 anos")
 print(f"A media de idade do grupo é de {media} anos")
+print(f"A maior idade é de {maior_idade}")
