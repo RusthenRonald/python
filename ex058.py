a2ef16b50f79e0ced6655b23e20ecea25ae6e8ff
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-28 14:37:17 -0300

    finalizei exercicio

--- ex058.py
+++ ex058.py
@@ -1,6 +1,8 @@
-
+tot=soma=0
 num=int(input("Digite um número inteiro: (999) para interromper"))
 while num != 999:
+    tot+=1
+    soma+=num
     num=int(input("Digite um número inteiro: (999) para interromper"))
-
 print("FIM")
+print(f"Foram digitados {tot} números e a soma entre eles foi de {soma}")
