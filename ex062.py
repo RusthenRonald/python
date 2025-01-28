30776aa8ab5659fa12d53458f2558e5f9718ec6b
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-28 17:27:34 -0300

    finalizei programa

--- ex062.py
+++ ex062.py
@@ -1,7 +1,25 @@
 import random
+v=0
 while True:
     num=int(input("Digite um valor:"))
     opc=str(input("Digite par ou impar (P/I):"))
+    while opc not in "PpIi":
+        opc=str(input("Digite par ou impar (P/I):"))
     computador=random.randint(0,10)
     total=num+computador
-    print(f"Você jogou {num} e o computador {computador}.")
+    print(f"Você jogou {num} e o computador {computador}. Total de {total}")
+    if opc in "Pp":
+        if total%2==0:
+            print("Você vençeu!")
+            v+=1
+        else:
+            print("Você perdeu!")
+            break
+    elif opc in "Ii":
+        if total%2==1:
+            print("Você vençeu!")
+            v+=1
+        else:
+            print("Você perdeu!")
+            break
+print(f"Fim. Você venceu {v} vezes")
