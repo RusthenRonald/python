26e22a6c7c367c9df85012ca0bcfbdd14b74657a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-28 16:39:28 -0300

    interromprendo laço quando numero for negativo

--- ex061.py
+++ ex061.py
@@ -1,5 +1,9 @@
-num=int(input('Digite um número para saber sua tabuada:'))
-c=0
-while c <=10:
-    print(f"{num}x{c}={num*c}")
-    c+=1
+while True:
+    num=int(input('Digite um número para saber sua tabuada:'))
+    if num<0:
+        print("Só aceito números positivos.Programa encerrando...")
+        break
+    c=0
+    while c <=10:
+        print(f"{num}x{c}={num*c}")
+        c+=1
