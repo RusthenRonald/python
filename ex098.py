764728c4f750216fb4605fa2e047f7fc57101c1f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-29 17:00:20 -0300

    finalizei programa

--- ex098.py
+++ ex098.py
@@ -6,7 +6,7 @@ valores=(
 )
 print(valores)
 print(f"O valor 9 apareceu {valores.count(9)} vezes")
-print(f"O primeiro valor 3 foi digitado na posição {valores.index(3)}°")
+print(f"O primeiro valor 3 foi digitado na posição {valores.index(3)+1}°")
 print("Os valores pares são:",end=" ")
 for v in valores:
     if v%2==0:
