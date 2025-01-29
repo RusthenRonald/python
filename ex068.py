bb893ec19bd8aceca5730cd8bceb8ceb86a659b1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-29 16:44:48 -0300

    usando max e min

--- ex068.py
+++ ex068.py
@@ -1,3 +1,5 @@
 import random
 valores=(random.randint(0,10),random.randint(0,10),random.randint(0,10),random.randint(0,10),random.randint(0,10))
 print(valores)
+print(f"O maior número é {max(valores)}")
+print(f"O menor número é {min(valores)}")
