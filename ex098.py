b616484d760cf154e5322eb6f4d3a92378113460
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-29 16:55:50 -0300

    usando count em tupla

---
+++ ex098.py
@@ -0,0 +1,8 @@
+valores=(
+    int(input("Digite um valor:")),
+    int(input("Digite um valor:")),
+    int(input("Digite um valor:")),
+    int(input("Digite um valor:"))
+)
+print(valores)
+print(f"O valor 9 apareceu {valores.count(9)} vezes")
