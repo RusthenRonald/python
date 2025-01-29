477edf1531b9df6ea0b5f8722145633c673eea42
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-01-29 15:00:24 -0300

    criando tupla com números por extenso

---
+++ ex066.py
@@ -0,0 +1,3 @@
+valores=("Zero","Um","Dois","Três","Quatro","Cinco","Seis","Sete","Oito","Nove","Dez","Onze","Doze","Treze","Quartorze","Quinze","Dezesseis","Dezesete","Dezoito","Dezenove","Vinte")
+num=int(input("Digite um número entre 0 e 20:"))
+print(f"Voc~e digitou o número {valores[num]}")
