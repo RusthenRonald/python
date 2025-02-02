543069e4f608b96e50d2c90ffabf85984ad1020b
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-02 02:20:12 -0300

    finalizei exercicio

--- ex076.py
+++ ex076.py
@@ -3,12 +3,11 @@ pares=[]
 impares=[]
 while True:
     num=int(input("Digite um valor:"))
+    valores.append(num)
     if num%2==0:
         pares.append(num)
-        valores.append(num)
     else:
         impares.append(num)
-        valores.append(num)
     r=str(input("Digite se quer continuar? (S/N):"))
     if r in "Nn":
         break
