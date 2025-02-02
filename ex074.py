ae3528fb97d481785e8a2912e4f31d6faa7775ac
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-02 01:14:29 -0300

    finalizei exercicio

--- ex074.py
+++ ex074.py
@@ -1,4 +1,15 @@
 valores=[]
 for c in range(1,6):
-    valores.append(int(input("Digite um valor:")))
-print(valores)
+    num=int(input("Digite um valor:"))
+    if c ==1:
+        valores.append(num)
+    elif num>valores[-1]:
+        valores.append(num)
+    else:
+        pos=0
+        while pos<len(valores):
+            if num<=valores[pos]:
+                valores.insert(pos,num)
+                break
+            pos+=1
+print(f"Os valores digitados em ordem foram {valores}")
