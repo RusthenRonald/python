d4ff2284a084097572275d203034aa1c104e3967
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-03 00:41:06 -0300

    sorted para ordenar numeros

--- ex078.py
+++ ex078.py
@@ -1,5 +1,10 @@
-valores=[]
+valores=[[],[]]
 for c in range(0,7):
     num=int(input(f"Digite o {c+1}° valor:"))
-    valores.append(num)
-print(valores)
+    if num%2==0:
+        valores[0].append(num)
+    elif num%2==1:
+        valores[1].append(num)
+print(f"Todos os valores foram {valores}")
+print(f"Os valores pares digitados foram {sorted(valores[0])}")
+print(f"Os valores impares digitados foram {sorted(valores[1])}")
