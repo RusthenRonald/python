3b2d9916a585f83c0369f0cf262c16b115fb19d6
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-04 17:23:10 -0300

    fazendo a soma de todos os valores pares

--- ex079.py
+++ ex079.py
@@ -1,8 +1,13 @@
+soma=0
 matriz=[[0,0,0],[0,0,0],[0,0,0]]
 for l in range(0,3):
     for c in range(0,3):
         matriz[l][c]=int(input(f"Digite um valor para [{l}],[{c}]:"))
 for l in range(0,3):
     for c in range(0,3):
+        if matriz[l][c]%2==0:
+            soma+=matriz[l][c]
         print(f"[{matriz[l][c]}]",end=" ")
     print()
+
+print(f"A soma de todos os valores pares foram {soma}")
