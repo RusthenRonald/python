7d0994a7b461c99cf696d8637acca91598efa6d4
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-05 22:39:14 -0300

    fazendo verificação de maior número

--- ex079.py
+++ ex079.py
@@ -1,8 +1,8 @@
-soma=somaterce=0
+soma=somaterce=maior=0
 matriz=[[0,0,0],[0,0,0],[0,0,0]]
 for l in range(0,3):
     for c in range(0,3):
-        matriz[l][c]=int(input(f"Digite um valor para [{l}]:[{c}]"))
+        matriz[l][c]=int(input(f"Digite um valor para [{l}][{c}] : "))
 for l in range(0,3):
     for c in range(0,3):
         if matriz[l][c]%2==0:
@@ -11,5 +11,12 @@ for l in range(0,3):
     print()
 for l in range(0,3):
     somaterce+=matriz[l][2]
+for c in range(0,3):
+    if c ==0:
+       maior=matriz[1][c]
+    else:
+        if matriz[1][c]>maior:
+            maior=matriz[1][c]
 print(f"A soma de todos os valores pares é de {soma}")
 print(f"A soma dos valores da terceira coluna é de {somaterce}")
+print(f"O maior valor da segunda linha é {maior}")
