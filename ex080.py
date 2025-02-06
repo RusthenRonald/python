b0593300f4090a00394e5a9d176c396f02af46b2
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-05 23:20:10 -0300

    adicionei sort pra ordenar valores

--- ex080.py
+++ ex080.py
@@ -5,6 +5,7 @@ valores=[]
 jogos=int(input("Quantos jogos serão gerados?"))
 for c in range(0,jogos):
     computador=random.sample(range(1,60),6)
+    valores.sort()
     valores.append(computador)
 print(f"Sorteando {jogos} jogos")
 for i,v in enumerate(valores):
