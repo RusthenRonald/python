ae40347deaf7dbedcfd16ee43b1888e158b36eeb
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-05 22:24:53 -0300

    mostrando posição do range

--- ex079.py
+++ ex079.py
@@ -1,7 +1,7 @@
 matriz=[[0,0,0],[0,0,0],[0,0,0]]
 for l in range(0,3):
     for c in range(0,3):
-        matriz[l][c]=int(input("Digite um valor:"))
+        matriz[l][c]=int(input(f"Digite um valor para [{l}]:[{c}]"))
 for l in range(0,3):
     for c in range(0,3):
         print(f"[{matriz[l][c]:^10}]",end=" ")
