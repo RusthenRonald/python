2d6c6960d180e11dbbc5d7d79bb310f832add06b
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-07 00:41:29 -0300

    finalizei programa

--- ex081.py
+++ ex081.py
@@ -1,12 +1,20 @@
 temp=[]
 principal=[]
+media=soma=0
 while True:
     temp.append(str(input("Digite seu nome:")))
     temp.append(int(input("Digite a primeira nota:")))
     temp.append(int(input("Digite a segunda nota:")))
+    media=(temp[1]+temp[2])/2
+    temp.append(media)
     principal.append(temp[:])
     temp.clear()
     r=str(input("Quer continuar? (S/N):"))
     if r in "Nn":
         break
-print(principal)
+print("Boletim")
+print("=-"*15)
+print(f"{"Pos"} {"Nome"} {"Média"}")
+for i,v in enumerate(principal):
+    print(f"{i} {v[0]} {v[3]}")
+print("=-"*15)
