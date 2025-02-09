abb110bae7f294b77648f3f09e1198c749bce578
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 02:14:38 -0300

    ultilizando bibilioteca operator para coletar items

--- ex083.py
+++ ex083.py
@@ -1,5 +1,14 @@
 import random
+import time
+import operator
 jogo={}
 for c in range(1,5):
     jogo[f"Jogador{c}"]=random.randint(1,6)
-print(jogo)
+for k,v in jogo.items():
+    print(f"{k} jogou o dado {v}")
+    time.sleep(1)
+print("=-"*15)
+print("RANKING DOS JOGADORES")
+ranking={}
+ranking=sorted(jogo.items(),key=operator.itemgetter(1),reverse=True)
+print(ranking)
