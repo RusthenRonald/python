760169ec7c4b414efb53abd9988b7e7bdc0327a5
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 01:41:02 -0300

    varrendo dicionario

--- ex082.py
+++ ex082.py
@@ -1,2 +1,12 @@
 aluno={}
-aluno["nome"]="Rusthen"
+aluno["nome"]=str(input("Nome:"))
+aluno["Media"]=float(input("Média:"))
+if aluno["Media"]>=7:
+    aluno["Situação"]="Aprovado"
+elif aluno["Media"]>=6 and aluno["Media"]<7:
+    aluno["Situação"]="Recuperação"
+else:
+    aluno["Situação"]="Reprovado"
+
+for k, v in aluno.items():
+    print(f"{k}: {v}")
