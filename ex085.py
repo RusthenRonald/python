066a978fbfb2b8b74b7e45ddcfb36313fa3613ea
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 20:30:04 -0300

    criando laço que inicie em 0 e varra o input partidas

--- ex085.py
+++ ex085.py
@@ -1,1 +1,5 @@
 jogador={}
+jogador["Nome"]=str(input("Nome do jogador:"))
+jogador["Partidas"]=int(input("Quantidade de partidas:"))
+for c in range(0,jogador["Partidas"]):
+    jogador["Gols"]=int(input(f"Quantos gols na partida {c+1}:"))
