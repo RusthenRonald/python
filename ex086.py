e66a4f27c5ca462ec264924768f63ccadb08eb8f
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 21:28:24 -0300

    adicionando contador de idades

--- ex086.py
+++ ex086.py
@@ -1,10 +1,13 @@
 dados={}
 principal=[]
-media=soma_idade=0
+media=soma_idade=tot_idade=0
 while True:
     dados["Nome"]=str(input("Nome:"))
     dados["Sexo"]=str(input("Sexo:"))
     dados["Idade"]=int(input("Idade:"))
+    soma_idade+=dados["Idade"]
+    tot_idade+=1
+    media=soma_idade/tot_idade
     principal.append(dados.copy())
     dados.clear()
     r=str(input("Quer continuar? (S/N):")).strip()
@@ -12,3 +15,4 @@ while True:
         break
 print(principal)
 print(f"Ao todo tem {len(principal)} pessoas cadastradas")
+print(f"A media total foi de {media}")
