e38891834715e5d7497813f1c76db33410369ca1
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 21:23:28 -0300

    calculando media e adicionando dicionario em lista

--- ex086.py
+++ ex086.py
@@ -1,2 +1,14 @@
-
+dados={}
+principal=[]
+media=soma_idade=0
 while True:
+    dados["Nome"]=str(input("Nome:"))
+    dados["Sexo"]=str(input("Sexo:"))
+    dados["Idade"]=int(input("Idade:"))
+    principal.append(dados.copy())
+    dados.clear()
+    r=str(input("Quer continuar? (S/N):")).strip()
+    if r in "Nn":
+        break
+print(principal)
+print(f"Ao todo tem {len(principal)} pessoas cadastradas")
