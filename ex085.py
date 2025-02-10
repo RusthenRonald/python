08f99a83a49fdde546c139ab31434563f8a9e33e
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 21:00:29 -0300

    ultilizando sum ao inves de criar acumulador de valores manualmente

--- ex085.py
+++ ex085.py
@@ -1,14 +1,12 @@
 jogador={}
 lista_gols=[]
-tot_gols=0
 jogador["Nome"]=str(input("Nome do jogador:"))
 jogador["Partidas"]=int(input("Quantidade de partidas:"))
 for c in range(0,jogador["Partidas"]):
     gols=int(input(f"Quantos gols na partida {c+1}:"))
     lista_gols.append(gols)
     jogador["Gols"]=lista_gols
-    tot_gols+=gols
-    jogador["Total de gols"]=tot_gols
+    jogador["Total de gols"]=sum(lista_gols)
 print("=-"*15)
 print(jogador)
 print("=-"*15)
