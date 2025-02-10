c3fe1e60abf8ee1a8d33d2d7c91ace456aef2d35
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-09 21:38:08 -0300

    exibindo nome de mulheres cadastradas

--- ex086.py
+++ ex086.py
@@ -1,9 +1,12 @@
 dados={}
 principal=[]
+lista_mulheres=[]
 media=soma_idade=tot_idade=0
 while True:
     dados["Nome"]=str(input("Nome:"))
     dados["Sexo"]=str(input("Sexo:"))
+    if dados["Sexo"] in "Ff":
+        lista_mulheres.append(dados["Nome"])
     dados["Idade"]=int(input("Idade:"))
     soma_idade+=dados["Idade"]
     tot_idade+=1
@@ -15,4 +18,7 @@ while True:
         break
 print(principal)
 print(f"Ao todo tem {len(principal)} pessoas cadastradas")
-print(f"A media total foi de {media} anos")
+print(f"A media total foi de {media:.1f} anos")
+print(f"As mulheres cadastradas foram ",end=" ")
+for m in lista_mulheres:
+    print(m,end=" ")
