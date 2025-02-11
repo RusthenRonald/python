138233ef0324f5c32bf53785ac2e4d7648f0f138
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-11 14:21:08 -0300

    apaguei teste

--- testeM3.py
+++
@@ -1,7 +0,0 @@
-lanche=("Hamburguer","Suco","Pizza","Pudim","Batata frita")
-for comida in lanche :#quando não precisa de posição
-    print(comida)
-for cont in range(len(lanche)):#quando precisa de posição
-    print(f"Eu vou comer {lanche[cont]} na posição {cont+1}")
-for pos,comida in enumerate(lanche) :#usando duas variaveis no for para achar posição
-    print(f"Vou comer {comida} na posição {pos+1}°")
