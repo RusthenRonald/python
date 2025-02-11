29943048583b26c5b823f74285d511df1fe18946
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-11 17:19:06 -0300

    adicionando valores ao dicionario

---
+++ ex087.py
@@ -0,0 +1,10 @@
+jogador={}
+gols=[]
+jogador["Nome do jogador:"]=str(input("Digite o nome do jogador"))
+partidas=int(input(f"Digite a quantidade de partidas que {jogador['Nome do jogador:']} jogou:"))
+for c in range(0,partidas):
+    gols_partidas=int(input(f"Quantos gols na partida {c+1}: "))
+    gols.append(gols_partidas)
+    jogador["Gols"]=gols
+    jogador["Total de gols"]=sum(gols)
+print(jogador)
