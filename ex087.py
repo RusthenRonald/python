c80a37282cc511ad51ab6655da58357336d51471
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-11 17:47:12 -0300

    adicionando dicionario a lista

--- ex087.py
+++ ex087.py
@@ -1,10 +1,19 @@
 jogador={}
 gols=[]
-jogador["Nome do jogador"]=str(input("Digite o nome do jogador:"))
-partidas=int(input(f"Digite a quantidade de partidas que {jogador['Nome do jogador']} jogou:"))
-for c in range(0,partidas):
-    gols_partidas=int(input(f"Quantos gols na partida {c+1}: "))
-    gols.append(gols_partidas)
-    jogador["Gols"]=gols
-    jogador["Total de gols"]=sum(gols)
+principal=[]
+while True:
+    jogador["Nome do jogador"]=str(input("Digite o nome do jogador:"))
+    partidas=int(input(f"Digite a quantidade de partidas que {jogador['Nome do jogador']} jogou:"))
+    for c in range(0,partidas):
+        gols_partidas=int(input(f"Quantos gols na partida {c+1}: "))
+        gols.append(gols_partidas)
+        jogador["Gols"]=gols
+        jogador["Total de gols"]=sum(gols)
+    principal.append(jogador.copy())
+    jogador.clear()
+    r=str(input("Digite se quer continuar (S/N):"))
+    if r in "Nn":
+        break
 print("=-"*15)
+for p in principal:
+    print(p)
