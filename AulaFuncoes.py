1362902f3487952887e9cc603ff0a1eedb742ef9
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-25 00:42:05 -0300

    testando empacotamento

--- AulaFuncoes.py
+++ AulaFuncoes.py
@@ -1,6 +1,7 @@
-def soma(a,b):
-    print(f'A = {a} e B = {b}')
-    soma=a+b
-    print(soma)
+def contador (*num):
+    tam=len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam}')
 
-soma(b=4,a=5)
+contador(2,3)
+contador(1,5,2,8)
+contador(2)
