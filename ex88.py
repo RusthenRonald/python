1e17864cc2379f1367bdc9e4bca69103ec36a8dc
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-25 01:10:16 -0300

    calculando área

---
+++ ex88.py
@@ -0,0 +1,7 @@
+def area(l,c):
+    area=l*c
+    print(f"A área de um terreno de {l}x{c} será de {area}m")
+
+l=float(input('Digite a largura:'))
+c=float(input('Digite o comprimento:'))
+area(l,c)
