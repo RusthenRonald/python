1028732ab16deb57cfecf9b3f4aa8530cd17b4e5
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-03-25 00:22:17 -0300

    criando função de soma

---
+++ AulaFuncoes.py
@@ -0,0 +1,5 @@
+def soma(a,b):
+    soma=a+b
+    print(soma)
+
+soma(2,2)
--- ex088.py
+++
@@ -1,8 +0,0 @@
-def mensagem(msg):
-    print("-"*30)
-    print(msg)
-    print("-"*30)
-
-mensagem('Curso em Video')
-mensagem('Aprenda Python')
-mensagem('Gustavo Guanabara')
